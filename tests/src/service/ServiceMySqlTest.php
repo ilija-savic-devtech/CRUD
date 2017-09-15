8b5c4573eefb46623d33412678747a8db5ebba2b
ilijasavic <ilija.savic@devtechgroup.com>
2017-09-15 10:39:29 +0200

    Added test for getOne method

--- tests/src/service/ServiceMySqlTest.php
+++ tests/src/service/ServiceMySqlTest.php
@@ -44,4 +44,34 @@ class ServiceMySqlTest extends TestCase
 
         $this->assertEquals($array,  $service->getAll());
     }
+
+    public function testGetOne(){
+        //Mock Objects
+        $mockDatabase= \Mockery::mock('database\DbConnection');
+        $mockPdo = \Mockery::mock('PDO');
+        $logger = new Logger('logs');
+        $stmtMock = \Mockery::mock('PDOStatement');
+
+        //Data for comparing return value from getOne method
+        $student = new Student();
+        $student->setId(1)->setName("Petar")->setSurname("Petrovic")->setIndexNo("123456")->setAddress("Njegoseva 14");
+        $student2 = new Student();
+        $student2->setId(2)->setName("Sava")->setSurname("Savanovic")->setIndexNo("789456")->setAddress("Savina 19");
+        $array[0] = $student;
+        $array[1] = $student2;
+        //Data for returning from fetchAll method from PDOStatement object
+        $rowset[0] = ['id'=>1, 'name'=>'Petar', 'surname'=>'Petrovic', 'indexno'=>123456, 'address'=>'Njegoseva 14'];
+        $rowset[1] = ['id'=>2, 'name'=>'Sava', 'surname'=>'Savanovic', 'indexno'=>789456, 'address'=>'Savina 19'];
+
+        $stmtMock->shouldReceive('execute')->andReturn(true);
+        $stmtMock->shouldReceive('bindParam')->andReturn(true);
+        $stmtMock->shouldReceive('fetch')->andReturn($rowset[0]);
+
+        $mockDatabase->shouldReceive('connectMySql')->andReturn($mockPdo);
+        $mockDatabase->shouldReceive('prepare')->andReturn($stmtMock);
+
+        $service = new ServiceMySql($mockDatabase, $logger);
+
+        $this->assertEquals($student, $service->getOne(1));
+    }
 }
